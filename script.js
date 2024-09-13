e6fce11a810a4fc7d9dc3e9286f57409279d0f3c
nitishsm2002 <nitishdhavan8@gmail.com>
2024-09-13 20:57:43 +0530

    commit

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function toggleMenu() {
   const menuLinks = document.querySelector('.menu-links');
   menuLinks.classList.toggle('open');
 }
+document.getElementById('audioControl').addEventListener('click', function() {
+  var audio = document.getElementById('backgroundAudio');
+
+  // Toggle mute/unmute
+  if (audio.muted) {
+    audio.muted = false;
+    this.textContent = 'Mute Audio';
+  } else {
+    audio.muted = true;
+    this.textContent = 'Unmute Audio';
+  }
+});
