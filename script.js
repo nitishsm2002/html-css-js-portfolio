603ff24673a37c38b3a15e096617147e00644137
nitishsm2002 <nitishdhavan8@gmail.com>
2024-09-13 18:54:38 +0530

    design change

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
+  const menuLinks = document.querySelector('.menu-links');
+  menuLinks.classList.toggle('open');
+}
