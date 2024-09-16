1f9bf82ccf9cac4b4ad80357893544052e50ba73
nitishsm2002 <nitishdhavan8@gmail.com>
2024-09-16 20:17:07 +0530

    script

--- script.js
+++ script.js
@@ -2,15 +2,3 @@ function toggleMenu() {
   const menu = document.querySelector('#hamburger-nav .menu-links');
   menu.classList.toggle('open');
 }
-document.getElementById('audioControl').addEventListener('click', function() {
-  var audio = document.getElementById('backgroundAudio');
-
-  // Toggle mute/unmute
-  if (audio.muted) {
-    audio.muted = false;
-    this.textContent = 'Mute Audio';
-  } else {
-    audio.muted = true;
-    this.textContent = 'Unmute Audio';
-  }
-});
